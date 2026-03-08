f79d3854c589d5df6c77c7e661a4536c518159a4
Mark Tao <marktao1@outlook.com>
2026-03-07 20:33:31 -0500

    hologram face, green background.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 // src/App.jsx
 import MovingFace from "./components/MovingFace";
+import FaceHologram from "./components/FaceHologram";
+
+
+
 
 export default function App() {
   return (
-    <div>
-      <MovingFace />
-    </div>
+    <main style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 32, marginBottom: 16 }}>Mark Tao</h1>
+      <FaceHologram />
+    </main>
   );
 }
